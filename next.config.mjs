c2e42bd596a18e9611d6842323192b3152f1e1f7
Noman Dhoni <92979541+nomandhoni-cs@users.noreply.github.com>
2026-06-26 22:40:31 +0600

    Override buildCommand to 'next build' in open-next.config.ts to break recursive loop

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,4 @@ const nextConfig = {};
 
 export default nextConfig;
 
-if (process.env.NODE_ENV === "development") {
-  import("@opennextjs/cloudflare").then((m) =>
-    m.initOpenNextCloudflareForDev()
-  );
-}
+import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
